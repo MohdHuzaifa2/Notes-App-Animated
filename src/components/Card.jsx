58795387414f012ef0cad9c71d6e8dd7c8e0e13b
MohdHuzaifa2 <mantasharuba81@gmail.com>
2026-02-08 11:54:59 +0530

    Added functionalities to the application

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,28 +1,43 @@
 import React from 'react'
-import { FileText, X, Download } from 'lucide-react'
+import { Trash2 } from 'lucide-react'
 import { motion } from "motion/react"
 
-const Card = ({ data, reference }) => {
+const Card = ({ data, obj, reference, setData }) => {
 
-    console.log(data.tag.color)
+    let options = {
+        month: "short",   // Feb
+        day: "2-digit",   // 06
+        hour: "2-digit",  // 02
+        minute: "2-digit",// 28
+        hour12: true,     // AM / PM
+    };
+
+    let formattedDate = new Intl.DateTimeFormat("en-US", options).format(new Date(obj.dateCreated))
+
+    const deleteNote = () => {
+        let arr = [...data];
+        arr.splice(data.indexOf(obj), 1)
+        setData(arr)
+
+        localStorage.setItem('data', JSON.stringify(arr));
+    }
 
     return (
-        <motion.div drag dragConstraints={reference} whileDrag={{ scale: 1.1 }} dragTransition={{
+        <motion.div drag dragConstraints={reference} whileDrag={{ scale: 1.05 }} dragTransition={{
             bounceStiffness: 100,
             bounceDamping: 10
-        }} dragElastic={0.9} className='w-66 h-72 rounded-4xl bg-zinc-900 text-zinc-200 tracking-wide flex flex-col overflow-hidden justify-between'>
-            <div className="content px-8 pt-8 flex flex-col gap-y-5">
-                <FileText size={20} strokeWidth={2.25} />
-                <p className='text-sm h-25 line-clamp-5 overflow-hidden'>{data.desc}</p>
+        }} dragElastic={0.9} className='w-66 h-72 rounded-4xl bg-zinc-900 text-zinc-200 tracking-wide flex flex-col overflow-hidden justify-between gap-y-5'>
+            <div className="content px-8 pt-8 flex flex-col gap-y-3">
+                <h2 className={`text-xl font-semibold ${obj.color}`}>{obj.title}</h2>
+                <p className='text-sm h-35 line-clamp-7 overflow-hidden'>{obj.desc}</p>
             </div>
             <div className="footer  flex flex-col text-[15px]">
                 <div className="flex px-8 gap-2 items-center justify-between mb-4">
-                    <span>{data.fileSize}</span>
-                    <button className="p-2.5 bg-zinc-600 rounded-full">
-                        {data.close ? <X size={16} strokeWidth={2.75} /> : <Download size={16} strokeWidth={2.75} />}
+                    <span className='text-zinc-400 text-sm'>{formattedDate}</span>
+                    <button onClick={deleteNote} className="p-2.5 bg-red-400/10 rounded-full cursor-pointer">
+                        <Trash2 size={20} color='#ff5252' strokeWidth={2.5} />
                     </button>
                 </div>
-                {data.tag.isOpen ? <button className={`tag p-3 cursor-pointer font-medium text-sm text-center ${data.tag.color}`}>{data.tag.title}</button> : null}
             </div>
         </motion.div>
     )
--- src/components/Foreground.jsx
+++ src/components/Foreground.jsx
@@ -1,30 +1,41 @@
-import React, { useRef, useState } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
+import { PlusIcon } from 'lucide-react'
 import Card from './Card'
 
 const Foreground = () => {
 
     let ref = useRef(null);
 
-    const [data, setData] = useState([
-    {
-      desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid, cum commodi provident pariatur cumque soluta dolorem",
-      fileSize: ".9mb",
-      close: true,
-      tag: {isOpen: true, title: "Download Now", color: "bg-blue-600"}
-    },
-    {
-      desc: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Pariatur, eveniet.",
-      fileSize: ".4mb",
-      close: false,
-      tag: {isOpen: true, title: "Upload Now", color: "bg-green-600"}
-    },
-  ])
+    const [data, setData] = useState([])
+
+    let btnHandler = () => {
+        let title = prompt("Enter note's title");
+        let desc = prompt("Enter notes description")
+
+        if (title && desc) {
+            setData(prev => (
+                [...prev, { id: crypto.randomUUID(), title: title, desc: desc, dateCreated: new Date(), color: "text-red-500" }]
+            ))
+        }
+
+        localStorage.setItem('data', JSON.stringify([...data, { id: crypto.randomUUID(), title: title, desc: desc, dateCreated: new Date(), color: "text-red-500" }]))
+    }
+
+    useEffect(() => {
+        if (localStorage.getItem('data')) {
+            let dataFromLocalStorage = JSON.parse(localStorage.getItem('data'));
+            setData([...dataFromLocalStorage])
+        }
+    }, [])
 
     return (
-        <div ref={ref} className="fixed inset-0 h-full p-5 flex flex-wrap gap-5 content-start">
-            {data.map((data, index) => (
-                <Card key={index} data={data} reference={ref} />
+        <div ref={ref} className="foreground fixed inset-0 h-full p-5 flex flex-wrap gap-5 content-start overflow-auto">
+            {data.map((obj, index) => (
+                <Card key={index} data={data} obj={obj} reference={ref} setData={setData} />
             ))}
+            <button onClick={btnHandler} className="create p-4 bg-green-400 rounded-full fixed bottom-12 right-12 z-10 text-zinc-900">
+                <PlusIcon size={48} strokeWidth={2.25} />
+            </button>
         </div>
     )
 }
